a85936502e74ffcfaf92322aa5a51f1c1848c834
thomas <89986227+thomas-te@users.noreply.github.com>
2023-11-07 20:01:06 -0500

    Update and rename te_flow1.py to my_flow.py

--- my_flow.py
+++ my_flow.py
@@ -1,9 +1,10 @@
 from prefect import flow
 
 @flow
-def sentiment_flow():
+def my_flow():
     return "thomas just did managed exec!"
+    
 
 if __name__ == '__main__':
-    flow.from_source("https://github.com/thomas-te/charlatan.git", entrypoint="te_flow1.py:sentiment_flow"
+    flow.from_source("https://github.com/thomas-te/charlatan.git", entrypoint="my_flow.py:my_flow"
                      ).deploy(name="prov-deployment", work_pool_name="managed-exec")
