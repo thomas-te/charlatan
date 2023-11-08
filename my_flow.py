f43ccf8dd05f5c372f8f0d3bd29460c0327d825c
thomas <89986227+thomas-te@users.noreply.github.com>
2023-11-07 21:57:21 -0500

    Update my_flow.py

--- my_flow.py
+++ my_flow.py
@@ -2,7 +2,8 @@ from prefect import flow, task
 
 @task
 def my_task():
-    return "thomas just did managed exec!"
+    phrase = "thomas just did managed exec!"
+    return phrase
     
 @flow(log_prints=True)
 def my_flow():
