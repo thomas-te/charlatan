c4da68511e9af594359e3dcbf90c6e87c20fff4b
thomas <89986227+thomas-te@users.noreply.github.com>
2023-11-07 19:58:09 -0500

    Update te_flow1.py

--- te_flow1.py
+++ te_flow1.py
@@ -1,5 +1,8 @@
 from prefect import flow
 
+@flow
+def sentiment_flow():
+    return "thomas just did managed exec!"
 
 if __name__ == '__main__':
     flow.from_source("https://github.com/thomas-te/charlatan.git", entrypoint="te_flow1.py:sentiment_flow"
