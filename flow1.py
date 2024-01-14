f95bdf4dd8b339cb9ec61c99103455943536f0c9
thomas <89986227+thomas-te@users.noreply.github.com>
2024-01-14 12:42:22 -0500

    Update flow1.py

--- flow1.py
+++ flow1.py
@@ -5,7 +5,7 @@ from prefect.artifacts import create_markdown_artifact
 def my_task():
     word = 'hey good job'
     create_markdown_artifact(key='report', markdown=word, description='resulting content')
-
+    
 
 @flow(log_prints=True)
 def my_flow():
